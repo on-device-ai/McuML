38b37a49b4164726699975a9f8dcb9fc3d056775
Yi-Lin Tung <yilintung@on-device-ai.com>
2021-02-23 20:13:21 +0800

    Corresponding to Anaconda
    If Anaconda is already installed on Windows. McuML will use Anaconda to build the environment. The built-in Miniconda will not be used.

--- uninstall.bat
+++ uninstall.bat
@@ -1,16 +1,21 @@
-@REM Copyright (C) 2012 Anaconda, Inc
-@REM SPDX-License-Identifier: BSD-3-Clause
-@CALL "%~dp0\third_party_tools\miniconda3\condabin\conda_hook.bat"
-@CALL conda.bat activate %*
-
-@REM *** McuML Project ***
-@REM Copyright (C) 2020 On-Device AI Co., Ltd.
-@CALL conda env remove --name McuML
-@CALL conda env remove --name mbed
-@rd /S /Q .\third_party_tools\miniconda3\envs
-@mkdir .\third_party_tools\miniconda3\envs
-@rd /S /Q .\tensorflow\tensorflow\lite\micro\tools\make\gen
-@rd /S /Q %userprofile%\.conda
-@rd /S /Q %userprofile%\.ipython
-@rd /S /Q %userprofile%\.jupyter
-@rd /S /Q %userprofile%\.mbed
+@check_anaconda_installed.exe
+@IF %ERRORLEVEL% EQU 0 GOTO StartMiniconda
+  @cmd.exe /c conda > NUL  2>NUL
+  @IF %ERRORLEVEL% EQU 0 GOTO UninstallMcuML
+  @ECHO The conda command not found
+  @GOTO END
+:StartMiniconda
+  @REM Copyright (C) 2012 Anaconda, Inc
+  @REM SPDX-License-Identifier: BSD-3-Clause
+  @CALL "%~dp0\third_party_tools\Miniconda3\condabin\conda_hook.bat"
+  @CALL conda.bat activate %*
+:UninstallMcuML
+  @REM *** McuML Project ***
+  @REM Copyright (C) 2020 On-Device AI Co., Ltd.
+  @CALL conda env remove --name McuML
+  @CALL conda env remove --name mbed
+  @rd /S /Q .\third_party_tools\miniconda3\envs
+  @mkdir .\third_party_tools\miniconda3\envs
+  @rd /S /Q .\tensorflow\tensorflow\lite\micro\tools\make\gen
+  @rd /S /Q %userprofile%\.conda
+:END
